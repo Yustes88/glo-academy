d581afd2604a3ae7d9777c9755847dccfa4eb451
Iuliia <iuliiaobr.io@gmail.com>
2024-04-18 18:16:44 +0700

    added variables and displayed alert and console messages

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("test");
+const title = "title";
+const screens = "screens";
+const screenPrice = "screenPrice";
+const rollback = "rollback";
+const fullPrice = "fullPrice";
+const adaptive = "adaptoive";
+
+alert("This is a first alert message");
+console.log("this is a first console text");
