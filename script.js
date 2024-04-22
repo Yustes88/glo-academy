6256e241031803788f794c4f8e7d62e7a9f0ff5d
Iuliia <iuliiaobr.io@gmail.com>
2024-04-22 16:39:01 +0700

    feat:displayed servicePercentPrice and switch result

--- script.js
+++ script.js
@@ -1,29 +1,69 @@
-const title = "Glo Academy";
+"use strict";
+
+/*const title = "Glo Academy";
 const screens = "Простые, Сложные, Интерактивные";
 const screenPrice = 1000;
-const rollback = 28;
 const fullPrice = 1000000;
 const adaptive = true;
 
-alert("This is a first alert message");
+// alert("This is a first alert message");
 console.log("this is a first console text");
 
-console.log(
-  `Title type is ${typeof title}, full price type is ${typeof fullPrice}, adaptive type is ${typeof adaptive}`
+// console.log(
+  //   `Title type is ${typeof title}, full price type is ${typeof fullPrice}, adaptive type is ${typeof adaptive}`
+  // );
+  
+  console.log(screens.length);
+  
+  // console.log("Стоимость вёрстки экранов " + screenPrice + " рублей/долларов/гривен/юани"); или
+  console.log(
+    `Стоимость вёрстки экранов ${screenPrice} рублей/долларов/гривен/юани`
+  );
+  
+  // console.log("Стоимость разработки сайт " + fullPrice + " рублей/долларов/гривен/юани"); или
+  console.log(
+    `Стоимость разработки сайта ${fullPrice} рублей/долларов/гривен/юани`
+  );
+
+  console.log(screens.toLowerCase().split(" "));
+  
+  console.log(fullPrice * (rollback / 100) + "%");
+  */
+
+// Lesson03
+const rollback = 28;
+const title = prompt("Как называется ваш проект?");
+
+const screens = prompt(
+  "Какие типы экранов нужно разработать?",
+  "Простые, Сложные, Интерактивные"
 );
 
-console.log(screens.length);
+const screenPrice = +prompt("Сколько будет стоить данная работа?", "12000");
 
-// console.log("Стоимость вёрстки экранов " + screenPrice + " рублей/долларов/гривен/юани"); или
-console.log(
-  `Стоимость вёрстки экранов ${screenPrice} рублей/долларов/гривен/юани`
-);
+const adaptive = confirm("Нужен ли адаптив на сайте?");
 
-// console.log("Стоимость разработки сайт " + fullPrice + " рублей/долларов/гривен/юани"); или
-console.log(
-  `Стоимость разработки сайта ${fullPrice} рублей/долларов/гривен/юани`
-);
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = +prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = +prompt("Сколько это будет стоить?");
 
-console.log(screens.toLowerCase().split(" "));
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
-console.log(fullPrice * (rollback / 100) + "%");
+const servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
+
+console.log(servicePercentPrice);
+
+switch (true) {
+  case fullPrice > 30000:
+    console.log("Даем скидку в 10%");
+    break;
+  case fullPrice > 15000 && fullPrice < 30000:
+    console.log("“Даем скидку в 5%");
+    break;
+  case fullPrice < 0:
+    console.log("Что то пошло не так");
+    break;
+  default:
+    console.log("Total price либо 0, либо 15000, либо 30000, либо не число");
+}
