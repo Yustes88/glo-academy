2c33aec281e1217a8baffc0548dfda63e325e513
Iuliia <iuliiaobr.io@gmail.com>
2024-05-02 18:10:37 +0700

    ordered books

--- js/script.js
+++ js/script.js
@@ -1,39 +1,20 @@
-'use strict'
-// Lesson09
-const mainTitle = document.getElementsByTagName('h1')[0]
+"use strict";
+// Lesson10
+const booksListContainer = document.querySelectorAll(".books");
+const booksList = document.querySelectorAll(".book");
+console.log(booksListContainer, booksList);
 
-const startBtn = document.getElementsByClassName('handler_btn')[0]
-const resetBtn = document.getElementsByClassName('handler_btn')[1]
-const screenBtn = document.querySelector('.screen-btn')
+const firstBookElement = booksList[1];
+const secondBookElement = booksList[0];
+const thirdBookElement = booksList[4];
+const forthBookElement = booksList[3];
+const fifthBookElement = booksList[6];
+const sixthBookElement = booksList[2];
 
-const itemsPercent = document.querySelectorAll('.other-items.percent')
-const itemsNumber = document.querySelectorAll('.other-items.number')
+const orderBooksList = function () {
+  booksListContainer[0].prepend(firstBookElement);
+  booksListContainer[0].append(sixthBookElement);
+  forthBookElement.before(thirdBookElement);
+};
 
-const inputRange = document.querySelector('.rollback input[type="range"]')
-
-const rangeValue = document.querySelector('.rollback span')
-
-const totalInput = document.getElementsByClassName('total-input')
-
-let screenBlocks = document.querySelectorAll('.screen')
-
-const displayInputs = function (list) {
-  for (let i = 0; i < list.length; i++) {
-    console.log(list[i])
-  }
-}
-
-displayInputs(totalInput)
-
-console.log(
-  mainTitle,
-  startBtn,
-  resetBtn,
-  screenBtn,
-  itemsPercent,
-  itemsNumber,
-  inputRange,
-  rangeValue,
-  totalInput,
-  screenBlocks,
-)
+orderBooksList();
